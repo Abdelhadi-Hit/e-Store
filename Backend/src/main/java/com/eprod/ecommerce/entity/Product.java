8089eb91070fdc11adb3fbdcf519f95dc3ae368b
ABDELHADI BAKHTY <abdelhadibakhty@ABDELHADIs-MacBook-Pro.local>
2023-10-15 22:53:10 +0100

    connect Product details to their images

--- Backend/src/main/java/com/eprod/ecommerce/entity/Product.java
+++ Backend/src/main/java/com/eprod/ecommerce/entity/Product.java
@@ -1,9 +1,7 @@
 package com.eprod.ecommerce.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Product {
@@ -14,6 +12,20 @@ public class Product {
     private String productDescription;
     private Double productDiscountedPrice;
     private Double productActualPrice;
+
+    @ManyToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+
+    @JoinTable(name = "product_images",
+            joinColumns = {
+              @JoinColumn(name = "product_id")
+            },
+            inverseJoinColumns = {
+              @JoinColumn(name = "image_id")
+            }
+
+    )
+
+    private Set<ImageModel> productImages ;
 
     public Integer getProductId() {
         return productId;
